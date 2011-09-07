fbec31992dd45177b3a3ce2d59a6dca83e768cb8
manishmsn <manish.guaranga@gmail.com>
2011-09-08 00:35:53 +0530

    Modified the code for ubuntu

--- battery.c
+++ battery.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 
-const char*  ENERGY_NOW = "/sys/class/power_supply/BAT0/energy_now";
-const char* ENERGY_FULL = "/sys/class/power_supply/BAT0/energy_full";
+const char*  ENERGY_NOW = "/sys/class/power_supply/BAT0/charge_now";
+const char* ENERGY_FULL = "/sys/class/power_supply/BAT0/charge_full";
 const char*      STATUS = "/sys/class/power_supply/BAT0/status";
 
 int main()
@@ -10,23 +10,24 @@ int main()
   int energy_now;
   int energy_full;
   char status[16];
-  
+
   FILE* f = fopen(ENERGY_NOW, "r");
   fscanf(f, "%d", &energy_now);
   fclose(f);
-  
-  
+
+
   f = fopen(ENERGY_FULL, "r");
   fscanf(f, "%d", &energy_full);
   fclose(f);
-  
+
   f = fopen(STATUS, "r");
   fscanf(f, "%s", status);
   fclose(f);
-  
+
   float cap = (float) energy_now / (float) energy_full * 100;
-  
+
   printf("Battery at %.0f%% (%s)\n", cap, status);
-  
+
   return 0;
 }
+
